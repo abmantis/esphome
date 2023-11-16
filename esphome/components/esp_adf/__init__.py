aac3d6a2c24aaed55a0e0f817aef7caa40cecbe2
abmantis <amfcalt@gmail.com>
2023-11-16 00:06:57 +0000

    Add Korvo1

--- esphome/components/esp_adf/__init__.py
+++ esphome/components/esp_adf/__init__.py
@@ -19,6 +19,7 @@ ESPADF = esp_adf_ns.class_("ESPADF", cg.Component)
 ESPADFPipeline = esp_adf_ns.class_("ESPADFPipeline", cg.Parented.template(ESPADF))
 
 SUPPORTED_BOARDS = {
+    "esp32s3korvo1": "CONFIG_ESP32_S3_KORVO1_BOARD",
     "esp32s3box": "CONFIG_ESP32_S3_BOX_BOARD",
     "esp32s3boxlite": "CONFIG_ESP32_S3_BOX_LITE_BOARD",
     "esp32s3box3": "CONFIG_ESP32_S3_BOX_3_BOARD",
